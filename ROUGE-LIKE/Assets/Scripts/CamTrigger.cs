5901a124d3a2f9449e2b4a0939a85b454b377d69
WizzCracker <rytwinski.karol@gmail.com>
2024-09-06 22:40:15 +0200

    First mechanics
    Player movement
    Camera movement beetwen rooms
    Sometimes camera doesn't work

---
+++ ROUGE-LIKE/Assets/Scripts/CamTrigger.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTrigger : MonoBehaviour
+{
+    public Vector3 newCamPos, newPlayerPos;
+
+    CamController camControl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camControl = Camera.main.GetComponent<CamController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            camControl.minPos += newCamPos;
+            camControl.maxPos += newCamPos;
+
+            collision.transform.position += newPlayerPos;
+        }
+    }
+}
