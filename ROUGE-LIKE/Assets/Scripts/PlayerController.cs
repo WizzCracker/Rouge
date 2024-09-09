bc69facbd0dfb34422a01ac726d5f2a2fba52da4
WizzCracker <rytwinski.karol@gmail.com>
2024-09-09 21:42:20 +0200

    Added player shooting

--- ROUGE-LIKE/Assets/Scripts/PlayerController.cs
+++ ROUGE-LIKE/Assets/Scripts/PlayerController.cs
@@ -7,17 +7,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField] public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
 
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.deltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
